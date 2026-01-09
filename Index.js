7471d4f4ed9459628ba7be1a67a2d9151afb4a9a
nisha48 <nishadhi899@gmail.com>
2026-01-09 13:15:43 +0530

    Update Index.js

--- Index.js
+++ Index.js
@@ -1,17 +1,14 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
-
-app.use(express.json());
+app.use(cors());
 
 app.get("/", (req, res) => {
-  res.json({
-    status: "OK",
-    service: "SquidFlix Backend",
-  });
+  res.send("SquidFlix Backend is LIVE 🚀");
 });
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log("Server running on port " + PORT);
 });
